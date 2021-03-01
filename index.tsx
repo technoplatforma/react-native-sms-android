f1568cbc43677835007c498755e28d71e1b67048
SergeyMild <sergeymild@yandex.ru>
2021-03-01 16:47:34 +0200

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,9 +1,13 @@
-import { NativeModules } from 'react-native'
+import { NativeModules, Linking, Platform } from 'react-native'
 
 export class SmsAndroid {
 
   static sms = async (phone: string, body: string): Promise<boolean> => {
-    return NativeModules.sms(phone, body)
+    if (Platform.OS === 'ios') {
+      Linking.openURL(`sms:&addresses=${phone}&body=${body}`)
+      return true
+    }
+    return NativeModules.SmsAndroid.sms(phone, body)
   }
 
 }
